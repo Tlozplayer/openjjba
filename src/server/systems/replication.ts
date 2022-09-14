73f4ece24bf807014bd8107494a30e5ad5a3872d
Mistrustfully <mistrustfully@gmail.com>
2022-09-14 06:29:38 -0600

    Updated Map

--- src/server/systems/replication.ts
+++ src/server/systems/replication.ts
@@ -11,6 +11,7 @@ import {
 	Renderable,
 	StandRig,
 	PlayerLike,
+	Dodging,
 } from "shared/components";
 import Remotes, { ComponentPayload } from "shared/remotes";
 import type { ComponentNames } from "shared/types/serde";
@@ -25,6 +26,7 @@ const ReplicatedComponents = new Set<ComponentCtor>([
 	StandRig,
 	ItemComponent,
 	Blocking,
+	Dodging,
 ]);
 
 function Replication(world: World): void {
